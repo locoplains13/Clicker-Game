0a3f4454a52324742a2b739a5a25939de9d982e0
Angel Roberto Andrade <alroc1234@hotmail.com>
2023-03-09 19:05:29 -0600

    buttons appear as you get price they're worth

--- Assets/Scripts/Clicker.cs
+++ Assets/Scripts/Clicker.cs
@@ -10,6 +10,9 @@ public class Clicker : MonoBehaviour
     public float machines;
     public float MachinePerSecond;
     public float x;
+    public GameObject[] buyButtons;
+    public int[] buyprices;
+    public int numMachine = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,19 +25,39 @@ public class Clicker : MonoBehaviour
     void Update()
     {
         scoreText.text = "Machines: "+ machines;
-        machinePerSecondText.text = "Codys/s: " + MachinePerSecond*100;
+        machinePerSecondText.text = "Machine/s: " + MachinePerSecond*100;
         MachinePerSecond = x * Time.deltaTime;
         machines += MachinePerSecond;
+        if (machines > buyprices[numMachine])
+        {
+            buyButtons[numMachine].SetActive(true);
+            numMachine++;
+        }
     }
 
     public void click(){
         machines += 1;
     }
-
+    
     public void BuyCursor(){
         if(machines >= 15){
             x += 1f;
             machines = machines - 15f;
         }
     }
+
+    public void BuyBitcoin(){
+        if(machines >= 500){
+            x += 50f;
+            machines = machines - 500f;
+        }
+    }
+    
+    public void BuyNasa(){
+        if(machines >= 10000){
+            x += 1000f;
+            machines = machines - 10000f;
+        }
+    }
+    
 }
